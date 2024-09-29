15012f2fab5a93f80aeeee00f6d6508764ec51d3
Pufiadagaddt <kevinik0306@gmail.com>
2024-09-29 10:36:08 +0200

    v0.3

---
+++ Felaldozhatok/Assets/EnemyManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyManager : MonoBehaviour
+{
+    public Text missionText; // A felirat megjelenítéséhez
+    private int enemiesKilled = 0; // A megölt ellenségek száma
+    private int maxEnemies; // A maximálisan spawnolható ellenségek száma
+    public static EnemyManager instance; // Singleton referencia
+
+    void Awake()
+    {
+        // Singleton biztosítása
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // A maximális ellenségszám beállítása (a SpawningEnemy.cs-ből hívjuk meg)
+    public void SetMaxEnemies(int max)
+    {
+        maxEnemies = max;
+    }
+
+    // Az ellenségek halálának kezelése
+    public void EnemyKilled()
+    {
+        enemiesKilled++;
+        if (enemiesKilled >= maxEnemies) // Csak akkor írja ki, ha az összes spawnolt ellenség meghalt
+        {
+            MissionSuccess();
+        }
+    }
+
+    // "Mission Successful" felirat kiírása
+    void MissionSuccess()
+    {
+        missionText.text = "Mission Successful!";
+    }
+}
